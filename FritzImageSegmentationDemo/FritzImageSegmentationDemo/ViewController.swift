2b569b399897676b05332f45e2ead372b83114d9
Chris Kelly <ghop02@gmail.com>
2018-10-09 10:32:46 -0400

    Step 1: Create Xcode project

---
+++ FritzImageSegmentationDemo/FritzImageSegmentationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FritzImageSegmentationDemo
+//
+//  Created by Christopher Kelly on 10/9/18.
+//  Copyright © 2018 Fritz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
